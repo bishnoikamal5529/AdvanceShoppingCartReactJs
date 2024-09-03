b6c5f495b406eafe98a3fc7239c320be96d9a00a
Kamal <bishnoikamal5529@gmail.com>
2024-09-03 17:30:53 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Advance Shopping Cart
+    </>
+  )
+}
+
+export default App
