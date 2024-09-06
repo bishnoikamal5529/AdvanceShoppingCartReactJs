479986fc276eb25536599d86c559350d326469a4
bishnoikamal5529 <52933842+bishnoikamal5529@users.noreply.github.com>
2024-09-06 16:49:29 +0000

    SlideIn added to store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,11 +1,13 @@
 import { Col, Row } from "react-bootstrap"
 import { StoreItem } from "../components/StoreItem"
 import storeItems from "../data/items.json"
+import "../CssFiles/slideInAnimation.css"
 
 export function Store() {
+  
   return (
     <>
-      <h1 className="text-center">Welcome To Store</h1>
+      <h1 className="slide-in text-center">Welcome To Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
