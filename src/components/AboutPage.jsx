7ab2249a55fa74ecec5df913927ff595cb9237c8
Kamal <bishnoikamal5529@gmail.com>
2024-09-16 19:57:03 -0400

    About page layout updated

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,7 +4,7 @@ export function AboutPage() {
 
   return <>
     <div className="blur-background background-image m-1">
-        <div className="content d-flex h-100 pt-3 flex-column flex-sm-row align-items-center justify-content-md-evenly justify-content-center">
+        <div className="content d-flex h-100 pt-3 flex-row align-items-center justify-content-md-evenly justify-content-center">
 
             <div className="overflow-hidden d-flex flex-column m-5 ms-2 ms-md-3 ms-lg-5">
                 <span className="text1 h1">Discover</span>
@@ -14,7 +14,7 @@ export function AboutPage() {
 
             <div className="overflow-hidden d-flex flex-column m-5 ms-2 ms-md-3 ms-lg-5">
                 <p className="h1 p-2 m-2 text1">Yes It's Public</p>
-                <a href="https://github.com/bishnoikamal5529/AdvanceShoppingCartReactJs.git" target="_blank" className="btn btn-dark px-5 py-2 m-2 text2">
+                <a href="https://github.com/bishnoikamal5529/AdvanceShoppingCartReactJs.git" target="_blank" className="btn btn-dark px-5 py-2 text-center text2">
                     GitHub
                 </a>
             </div>
